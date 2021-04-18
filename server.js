f0b6ea52dc86374ae29a9f07763f39f9a29d1ade
BigRob <robertcmoore92@gmail.com>
2021-04-18 13:52:42 -0400

    added case checker for different validated IDs

--- server.js
+++ server.js
@@ -18,23 +18,42 @@ console.log("Fizzbuzz number is " + numFizzBuzz);
 // N must be less than 6 digits
 
 function regexCheck(id, res) {
-    console.log("Regex Function Reached");
-    return /^[0-9]{1,5}$/.test(id);
-  }
+  console.log("Regex Function Reached");
+  return /^[0-9]{1,5}$/.test(id);
+}
 
+function fizzbuzzCaseChecker(id, res) {
+  console.log("fizzbuzzCaseChecker Reached");
+  switch (true) {
+    case id % numFizzBuzz == 0:
+      console.log("FizzBuzz returned");
+      res.send("FizzBuzz");
+      break;
+    case id % numFizz == 0:
+      console.log("Fizz returned");
+      res.send("Fizz");
+      break;
+    case id % numBuzz == 0:
+      console.log("Buzz returned");
+      res.send("Buzz");
+      break;
+
+    default:
+      console.log("Argument returned");
+      res.send(id.toString());
+  }
+}
 
 // routes
 app.get("/", function (req, res) {
-    res.send("Server Up");
-  });
-  
-  app.get("/Fizzbuzz", function (req, res) {
-    res.send("CandleScience Test Started");
-  });
-  
+  res.send("Server Up");
+});
+
+app.get("/Fizzbuzz", function (req, res) {
+  res.send("CandleScience Test Started");
+});
 
 // express listener
 app.listen(PORT, function () {
-    console.log("Listening on " + PORT);
-  });
-  
+  console.log("Listening on " + PORT);
+});
