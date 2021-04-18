a35e54c2394d01e3ec3d169459eb627aecc75444
BigRob <robertcmoore92@gmail.com>
2021-04-18 13:24:26 -0400

    Added regex function with constraints of number up to 5 digits

--- server.js
+++ server.js
@@ -13,6 +13,16 @@ console.log("Buzz number is " + numBuzz);
 var numFizzBuzz = numFizz * numBuzz;
 console.log("Fizzbuzz number is " + numFizzBuzz);
 
+// id rules
+// N must be a number
+// N must be less than 6 digits
+
+function regexCheck(id, res) {
+    console.log("Regex Function Reached");
+    return /^[0-9]{1,5}$/.test(id);
+  }
+
+
 // routes
 app.get("/", function (req, res) {
     res.send("Server Up");
