1bab0d07a122ce32f34059aa29a75897cd0732bc
BigRob <robertcmoore92@gmail.com>
2021-04-18 13:08:55 -0400

    added initial routes and express listener

--- server.js
+++ server.js
@@ -12,3 +12,19 @@ console.log("Buzz number is " + numBuzz);
 
 var numFizzBuzz = numFizz * numBuzz;
 console.log("Fizzbuzz number is " + numFizzBuzz);
+
+// routes
+app.get("/", function (req, res) {
+    res.send("Server Up");
+  });
+  
+  app.get("/Fizzbuzz", function (req, res) {
+    res.send("CandleScience Test Started");
+  });
+  
+
+// express listener
+app.listen(PORT, function () {
+    console.log("Listening on " + PORT);
+  });
+  
