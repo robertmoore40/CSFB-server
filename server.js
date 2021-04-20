48372187a358e03172d5bda872d9a387e979a68f
BigRob <robertcmoore92@gmail.com>
2021-04-19 22:07:32 -0400

    added second regex and integrated into general regex function, removed need for input check

--- server.js
+++ server.js
@@ -17,39 +17,74 @@ console.log("Fizzbuzz number is " + numFizzBuzz);
 // N must be a number
 // N must be less than 6 digits
 
-function regexCheck(id, res) {
-  console.log("Regex Function Reached");
-  // return /^[0-9]{1,5}$/.test(id);
-  return /^\d{1,5}$/.test(id);
-  
-// current issues
-// 315asd returns true
-// 15.5 returns true
-
-  // return /^\d+$/.test(id);
-  // Whole number checker
-
-}
-
-function inputCheck(id, res) {
-    console.log("Input Checker Reached");
-  
-    if (regexCheck(id, res)) {
-      console.log("Regex passed, fizzbuzz case checker started");
+function generalRegex(id, res) {
+  if (/^[0-9]*$/.test(id)) {
+    if (/^\d{1,5}$/.test(id)) {
       fizzbuzzCaseChecker(id, res);
     } else {
-      console.log(
-        "Regex Failed :Please enter a positive whole number up to 5 digits"
-      );
+      console.log("Second Regex Failed");
       res.send(
-        "Regex Failed :Please enter a positive whole number up to 5 digits"
+        "Second Regex Failed :Please limit the number to 5 digits"
       );
-    };
-  };
+    }
+  } else {
+    console.log("First Regex Failed");
+    res.send(
+      "First Regex Failed :Please enter a positive whole number up to 5 digits"
+    );
+  }
+}
+
+// function regexDigitCheck(id, res) {
+//   console.log("Regex Digit Checker Reached");
+//   return /^\d{1,5}$/.test(id);
+//   // allows numbers up to 5 digits
+//   // https://www.regextester.com/104020
+//   // 15.5 returns false online
+//   // 15.5 returns true in this regex
+//     // This should filter out decimals, perhaps periods are a partition in URL?
+// };
+
+// function regexStringCheck(id, res) {
+//   console.log("Regex String Checker Reached")
+//   return /^[0-9]*$/.test(id);
+//   // https://www.regextester.com/21
+//   // 23 returns true
+//   // 4389 returns true
+//   // y7ac8 returns false
+//   // bar returns false
+// };
+
+// function inputCheck(id, res) {
+//   console.log("Input Checker Reached");
+
+//   if (regexDigitCheck(id, res)) {
+//     console.log("First Regex Passed");
+//     if (regexStringCheck(id, res)) {
+//       console.log("Second Regex Passed");
+//       fizzbuzzCaseChecker(id, res);
+//     } else {
+//       console.log(
+//         "Second Regex Failed: Please enter a positive whole number up to 5 digits"
+//       );
+//     }
+//   } else {
+//     console.log(
+//       "First Regex Failed :Please enter a positive whole number up to 5 digits"
+//     );
+//     res.send(
+//       "First Regex Failed :Please enter a positive whole number up to 5 digits"
+//     );
+//   }
+// }
 
 function fizzbuzzCaseChecker(id, res) {
   console.log("fizzbuzzCaseChecker Reached");
   switch (true) {
+    case id == 0:
+      console.log("Not a valid input : Please pick a counting number");
+      res.send("Not a valid input : Please pick a counting number");
+      break;
     case id % numFizzBuzz == 0:
       console.log("FizzBuzz returned");
       res.send("FizzBuzz");
@@ -79,13 +114,13 @@ app.get("/Fizzbuzz", function (req, res) {
 });
 
 app.get("/Fizzbuzz/:id", function (req, res) {
-    var id = req.params.id;
-    console.log("New Input Detected");
-    console.log("__________");
-    console.log(id + " is the accepted parameter");
-    id = parseInt(id);
-    inputCheck(id, res);
-  });
+  var id = req.params.id;
+  console.log("New Input Detected");
+  console.log("__________");
+  console.log(id + " is the Input");
+  id = parseInt(id);
+  generalRegex(id, res);
+});
 
 // express listener
 app.listen(PORT, function () {
